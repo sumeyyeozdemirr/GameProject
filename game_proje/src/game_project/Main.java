d89ca6ab788514afaf99744d0587b017b34f9fe8
Sümeyye <Smeyye@DESKTOP-H18BTVK>
2021-05-05 17:19:01 +0300

    05.05.2021

--- game_proje/src/game_project/Main.java
+++ game_proje/src/game_project/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Logger[] loggers = {new EdevletLogger()};
 		CustomerManager customerManager = new CustomerManager(loggers);
 		
-		Customer sümeyye = new Customer(1,"Sümeyye","Özdemir",1997);
+		Customer sümeyye = new Customer(1,"Sümeyye ","Özdemir",1997);
 		customerManager.add(sümeyye);
 
 	}
